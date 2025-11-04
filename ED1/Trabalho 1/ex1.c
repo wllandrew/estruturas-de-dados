4c419aad14a4cc12436637db665ddd18c219793b
wllandrew <andrew.willian@uel.br>
2025-11-04 20:03:35 -0300

    feat: finalizacao trabalho

--- ED1/Trabalho 1/ex1.c
+++ ED1/Trabalho 1/ex1.c
@@ -1,85 +1,211 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
-#define INITIAL_SIZE 10
+typedef int ELEMENTO;
 
-typedef struct
+typedef struct Pilha
 {
-    int pilha[INITIAL_SIZE];
+    int *pilha;
+    int tamanho;
     int topo;
-    int extensions;
+    int aumentos;
 } Pilha;
 
-Pilha init_pilha()
+void InicializarPilha(Pilha *p)
 {
-    Pilha p;
-    p.topo = -1;
-    p.extensions = 0;
-    return p;
+    p->pilha = (int*)malloc(sizeof(int) * 10);
+    p->tamanho = 10;
+    p->topo = -1;
+    p->aumentos = 0;
 }
 
-void push(Pilha *p, int n)
+void PrintPilha(Pilha *p)
 {
-    if (p->topo < INITIAL_SIZE - 1)
+    for (int i=0; i<=p->topo; i++)
     {
-        p->topo++;
-        p->pilha[p->topo] = n;
+        printf("%d ", p->pilha[i]);
     }
-    return;
+    printf("\n");
 }
 
-void pop(Pilha *p)
+bool ProcuraPilha(Pilha *p, int valor, int *end)
 {
-    if (p->topo > -1)
+    for (int i=0; i<=p->topo; i++)
     {
-        p->pilha[p->topo] = 0;
-        p->topo--;
+        if (p->pilha[i] == valor)
+        {
+            *end = i;
+            return true;
+        }
     }
+    return false;
 }
 
-int len(Pilha *p)
+bool AppendPilha(Pilha *p, int valor)
 {
-    return p->topo + 1;
-}
-
-void percorre(Pilha *p)
-{
-    for (int i = p->topo; i > -1; i--)
+    if (p->topo >= p->tamanho)
     {
-        printf("Indice %d: %d\n", i, p->pilha[i]);
+        ResizePilha(p);
     }
+
+    p->pilha[++p->topo] = valor;
+
+    return true;
 }
 
-bool extendPilha(Pilha *p)
+bool RemovePilha(Pilha *p, int valor)
 {
-    if (p->extensions >= 3)
+    int end;
+    if (!ProcuraPilha(p, valor, &end))
     {
         return false;
     }
 
-    int size = sizeof(p->pilha) / sizeof(int);
+    for (int i=end; i<=p->topo; i++)
+    {
+        p->pilha[i] = p->pilha[i+1];
+    }
 
-    int copy[size];
-    for (int i=0; i<size; i++)
+    p->topo--;
+    return true;
+}
+
+bool ResizePilha(Pilha *p)
+{
+    int copy[p->tamanho];
+    for (int i=0; i<p->tamanho; i++)
     {
         copy[i] = p->pilha[i];
     }
 
-    size *= 3;
+    int tam = p->tamanho;
+    p->tamanho *= (int)1.5;
 
-    
-}
+    free(p->pilha);
 
-void reinit(Pilha *p)
-{
-    p->topo = -1;
+    p->pilha = (int*)malloc(sizeof(int) * p->tamanho);
+
+    for (int i=0; i<tam; i++)
+    {
+        p->pilha[i] = copy[i];
+    }
+    p->aumentos++;
 }
 
 int main()
 {
-    Pilha pilhas[2];
-    init_pilha(pilhas[0]);
-    init_pilha(pilhas[1]);
+    Pilha A[2];
+    InicializarPilha(&A[0]);
+    InicializarPilha(&A[1]);
 
+    int resp, valor, pilha, resultado;
+    while (true)
+    {
+        printf("0-sair\n");
+        printf("1-adicionar valor\n");
+        printf("2-remover valor\n");
+        printf("3-buscar valor\n");
+        printf("4-imprime pilha\n");
+        printf("5-destruir pilha\n");
 
+        scanf("%d", &resp);
+
+        if (resp == 0)
+        {
+            break;
+        }
+        else if (resp == 1)
+        {
+            printf("Valor: ");
+            scanf("%d", &valor);
+            printf("Pilha (1 ou 2): ");
+            scanf("%d", &pilha);
+            if (pilha != 1 && pilha != 2)
+            {
+                printf("Pilha invalida;\n");
+            }
+            else
+            {
+                resultado = AppendPilha(&A[pilha-1], valor);
+                if (!resultado)
+                {
+                    printf("Nao foi possivel adicionar a pilha\n");
+                }
+            }
+        }
+        else if (resp == 2)
+        {
+            printf("Valor: ");
+            scanf("%d", &valor);
+            printf("Pilha (1 ou 2): ");
+            scanf("%d", &pilha);
+            if (pilha != 1 && pilha != 2)
+            {
+                printf("Pilha invalida;\n");
+            }
+            else
+            {
+                resultado = RemovePilha(&A[pilha-1], valor);
+                if (!resultado)
+                {
+                    printf("Nao foi possivel remover da pilha\n");
+                }
+            }
+        }
+        else if (resp == 3)
+        {   
+            printf("Valor: ");
+            scanf("%d", &valor);
+            printf("Pilha (1 ou 2): ");
+            scanf("%d", &pilha);
+            if (pilha != 1 && pilha != 2)
+            {
+                printf("Pilha invalida;\n");
+            }
+            else
+            {
+                resultado = ProcuraPilha(&A[pilha-1], valor);
+                if (!resultado)
+                {
+                    printf("Valor nao esta na pilha\n");
+                }
+            }
+        }
+        else if (resp == 4)
+        {
+            printf("Pilha (1 ou 2): ");
+            scanf("%d", &pilha);
+            if (pilha != 1 && pilha != 2)
+            {
+                printf("Pilha invalida;\n");
+            }
+            else
+            {
+                PrintPilha(&A[pilha-1]);
+            }
+        }
+        else if (resp == 5)
+        {
+            printf("Pilha (1 ou 2): ");
+            scanf("%d", &pilha);
+            if (pilha != 1 && pilha != 2)
+            {
+                printf("Pilha invalida;\n");
+            }
+            else
+            {
+                free(&A[pilha-1]);
+            }
+        }
+
+        if (A[0].aumentos == 3 || A[1].aumentos == 3)
+        {
+            free(&A[0]);
+            free(&A[1]);
+            printf("Excedeu o limite de aumentos da pilha\n");
+        }
+    }
+
+    return 0;
 }
